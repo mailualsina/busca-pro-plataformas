1dded2dc519849d2b30ce2ecae03fec55c74e990
NineTravel <melanyascencio908@gmail.com>
2024-11-24 16:32:21 -0300

    feat: new component "Mi Perfil"

--- buscapro/src/App.js
+++ buscapro/src/App.js
@@ -3,20 +3,23 @@ import Login from "./components/Login";
 import Formulario from "./components/Formulario";
 import Register from "./components/Register";
 import Navbar from "./components/Navbar";
+import MiPerfil from "./components/MiPerfil";
+
 
 function App() {
   const [view, setView] = useState("login");
 
   return (
     <div>
-      {view === "login" && <Login setView={setView} />}
-      {view === "formulario" && <Formulario setView={setView} />}
+      {view === "login" && <Login setView={setView}/>}
+      {view === "fomulario" && <Formulario setView={setView} />}
       {view === "register" && <Register setView={setView} />}
       {view === "home" && (
         <div style={{ backgroundColor: "blue", height: "100vh" }}>
           <Navbar />
         </div>
       )}
+      {view === "miperfil" && <MiPerfil setView={setView} />}
     </div>
   );
 }
