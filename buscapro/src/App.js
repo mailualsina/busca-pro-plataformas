025ed965f0fc917fddc30b8e4f5f0ee32544fd90
NineTravel <melanyascencio908@gmail.com>
2024-12-01 15:23:11 -0300

    Fix: Fixed my profile navigation bug

--- buscapro/src/App.js
+++ buscapro/src/App.js
@@ -4,25 +4,28 @@ import Formulario from "./components/Formulario";
 import Register from "./components/Register";
 import Navbar from "./components/Navbar";
 import MiPerfil from "./components/MiPerfil";
-
 import ListaSolicitudes from "./components/ListaSolicitudes";
 
 
+
+
 function App() {
-  const [view, setView] = useState("listasolicitudes");
+  const [view, setView] = useState("home");
 
   return (
     <div>
       {view === "login" && <Login setView={setView}/>}
       {view === "fomulario" && <Formulario setView={setView} />}
       {view === "register" && <Register setView={setView} />}
+      {view === "miPerfil" && <MiPerfil setView={setView} />} 
+      {view === "listasolicitudes" && <ListaSolicitudes setView={setView} />}
+
       {view === "home" && (
         <div style={{ backgroundColor: "blue", height: "100vh" }}>
           <Navbar />
         </div>
       )}
-     {view === "miPerfil" && <MiPerfil setView={setView} />} 
-      {view === "listasolicitudes" && <ListaSolicitudes setView={setView} />}
+     
 
 
     </div>
