fad2ad0b3fea5cf3c1b15ee83c711ae1352a264b
glinco <glinco.dev@gmail.com>
2024-12-02 15:34:22 -0300

    Now using react router dom - Link -> Menu.js

--- buscapro/src/components/Menu.js
+++ buscapro/src/components/Menu.js
@@ -1,34 +1,37 @@
-export default function Menu() {
+import React from "react";
+import { Link } from "react-router-dom";
 
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">BuscaPro</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Login</a>
-                </li>
-                <li  className="nav-item">
-                <a className="nav-link" href="/register">Registro</a>
-                </li>
-                <li  className="nav-item">
-                <a className="nav-link" href="/miPerfil">Mi Perfil</a>
-                </li>
-                <li  className="nav-item">
-                <a className="nav-link" href="/listaSolicitudes">Solicitudes</a>
-                </li>
-                <li  className="nav-item">
-                <a className="nav-link" href="/listausuarios">Profesionales</a>
-                </li>
-            </ul>
-            </div>
+export default function Menu({ onLogout }) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">BuscaPro</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/miPerfil">Mi Perfil</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/listasolicitudes">Solicitudes</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/listausuarios">Profesionales</Link>
+            </li>
+          </ul>
+          <button className="btn" onClick={onLogout}>Logout</button>
         </div>
-        </nav>
-
-    ) 
+      </div>
+    </nav>
+  );
 }
-
