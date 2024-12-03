09bba7d6993dbd481acf80b4503edc9a3a71ff84
glinco <glinco.dev@gmail.com>
2024-12-03 18:47:27 -0300

    Configurando express para servir el build de frontend

--- server/server/index.js
+++ server/server/index.js
@@ -2,16 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql2");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const clientBuildPath = path.join(__dirname, "../../buscapro/build");
+app.use(express.static(clientBuildPath));
+
 // Create MySQL connection
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "lu21n39a",
   database: "buscapro"
 });
 
