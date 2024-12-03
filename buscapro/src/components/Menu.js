4cb77c07be1819bd14189d38227051bf84cd644f
NineTravel <melanyascencio908@gmail.com>
2024-12-03 12:44:27 -0300

    feat: add logo and icons to "menu"

--- buscapro/src/components/Menu.js
+++ buscapro/src/components/Menu.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.jpg";
+import "../styles/menu.css";
 
 export default function Menu({ onLogout }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">BuscaPro</Link>
+        <Link className="navbar-brand" to="/"> <img id="logo" src={logo} alt="Logo"/> </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -20,16 +22,17 @@ export default function Menu({ onLogout }) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to="/miPerfil">Mi Perfil</Link>
+              <Link className="nav-link" to="/miPerfil"><i className="fas fa-user"></i> Mi Perfil</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/listasolicitudes">Solicitudes</Link>
+              <Link className="nav-link" to="/listasolicitudes"><i className="fas fa-list"></i> Solicitudes</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/listausuarios">Profesionales</Link>
+              <Link className="nav-link" to="/listausuarios"><i className="fas fa-users"></i> Profesionales</Link>
             </li>
           </ul>
-          <button className="btn" onClick={onLogout}>Logout</button>
+          
+          <button className="btn" onClick={onLogout}><i className="fas fa-sign-out-alt"></i> Logout</button>
         </div>
       </div>
     </nav>
